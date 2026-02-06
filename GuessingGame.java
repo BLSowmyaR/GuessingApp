05c8b388e684b6651c18e9ef87f623b06aed337f
BLSowmyaR <sowmya.ravi@bridgelabz.com>
2026-02-06 17:51:20 +0530

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,16 +1,10 @@
 package com.guessingapp;
 
-import java.util.Random;
-
 public class GuessingGame {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Number Guessing Game!");
-
-        Random random = new Random();
-        int targetNumber = random.nextInt(100) + 1;
-
         System.out.println("A number has been generated between 1 and 100.");
     }
 }
