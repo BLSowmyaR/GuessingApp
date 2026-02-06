9c4e886fb5a4f88f377954b324e24b518c261935
BLSowmyaR <sowmya.ravi@bridgelabz.com>
2026-02-06 16:53:17 +0530

    [Sowmya] Added:UC1:welcome-message

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+package com.guessingapp;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Think of a number between 1 and 100.");
+    }
+}
